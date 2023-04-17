1d6b1bf4936ede1bb97700f08507d0c1e486a229
aki akenomyosei <101hsu5201@gmail.com>
2023-04-17 14:50:08 +0800

    2

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import json 
+import random
+
 
 with open ('setting.json','r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -16,17 +18,27 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1094865111119827034)
+    channel = bot.get_channel(int(jdata["welcome & leave"]))
     await channel.send(f'{member} join!')
-    
+
+@bot.command()
+async def helping(ctx):
+    await ctx.send(f'這是一個可愛的bot,但它甚麼都不會\n它現在只會:\nhelp:你正在看,不是嗎?\npic:傳一些無聊的圖片\nping:延遲好耶\np.s:製作者正在試圖讓它發出噪音,請趕快阻止他'
+)
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1094865111119827034)
+    channel = bot.get_channel(int(jdata["welcome & leave"]))
     await channel.send(f'{member} leave!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+@bot.command()
+async def pic(ctx):
+    random_pic = random.choice(jdata["pic"])
+    pic = discord.File(random_pic)
+    await ctx.send(file=pic)
 
 bot.run(jdata['TOKEN'])
